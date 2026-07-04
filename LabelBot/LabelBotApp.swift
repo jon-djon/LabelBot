a56c9dfb97ff317253e5438319c4afec38e24f08
Jon <jonmalm@gmail.com>
2026-07-04 12:12:51 -0400

    Initial Commit

---
+++ LabelBot/LabelBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelBotApp.swift
+//  LabelBot
+//
+//  Created by Jon on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LabelBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LabelBotTests/LabelBotTests.swift
@@ -0,0 +1,19 @@
+//
+//  LabelBotTests.swift
+//  LabelBotTests
+//
+//  Created by Jon on 7/4/26.
+//
+
+import Testing
+@testable import LabelBot
+
+struct LabelBotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
